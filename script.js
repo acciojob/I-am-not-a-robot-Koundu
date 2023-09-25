7bfd0d2d9c7f783c1fb18e0f5b931378c2a13585
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-25 16:36:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+min =1;
+max =5;
+var num = Math.floor(Math.random() * (max - min + 1)) + min;
+var imgName = "img"+num;
+const container = document.getElementsByClassName("flex")[0];
+const box = document.createElement("div");
+box.classList.add("img");
+container.appendChild(box);
+console.log(container,imgName)
